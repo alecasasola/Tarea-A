b72a6a5d20511e66c11a04ce9d6b7ee6b673fc92
casab <casab@ALE>
2023-10-02 19:10:40 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace Estudiante
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            //Hacer un algoritmo que calcule y solicite las notas de 4 estudiantes calcular cuantos estudiantes tienes nota superior al promedio
+
+            float[] notas = new float[4];
+            string[] nombre = new string[4];
+            float total = 0;
+
+        
+        }
+    }
+}
